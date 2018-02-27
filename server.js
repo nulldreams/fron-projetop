0b8390d7605e5b1eb12d3d3d58701861d8ba8561
unknown <igor.souza96@hotmail.com>
2018-02-27 20:53:16 -0300

    versão 0.0.1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const port = process.env.PORT || 80
+const port = process.env.PORT || 3001
 
 app.use(bodyParser.json())
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
